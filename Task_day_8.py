aa5b741a94cff2e53e554c9433e49536a12d7fae
soumya moharana <rysoumya@gmail.com>
2026-05-07 20:04:43 +0530

    Task_day_8.py

---
+++ Task_day_8.py
@@ -0,0 +1,63 @@
+#1. Parking Lot Monitor
+
+inp = [1, 1, 0, 1, 0, 0]
+
+
+
+
+'''inp =  "Learning Python is Good! coding Python"
+res = []
+for i in inp.split(' '):
+    if i.startswith('#'):
+        res.append(i)
+
+print(res)
+'''
+
+'''
+data1 = 'avd'
+res=''
+for i in data1:
+    res = res + chr(ord(i)+3)
+
+print(res)
+'''
+
+#Input Prices: [100, 500, 1500, 2500, 4000, 8000]
+#Min: 1000
+#Max: 3000
+#Output: [1500, 2500]
+
+'''Input_Prices = [100, 500, 1500, 2500, 4000, 8000]
+fin_price=[]
+inp_min = int(input("Enter min price: "))
+inp_max = int(input("Enter max price: "))
+
+for i in Input_Prices:
+    if i > inp_min and i < inp_max:
+        fin_price.append(i)
+
+print(fin_price)'''
+
+#6. Token Generation: Given a string of user info, generate a "Token" by taking
+#the first letter of each word and joining them.
+
+'''inp_str =input("Enter the string: ")
+res=""
+for i in inp_str.split(' '):
+    res+=i[0]
+
+
+print(res)'''
+
+
+
+
+
+
+
+
+
+
+
+
